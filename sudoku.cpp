33c816c5fad1589e1ecf44223fe4d21942dad5d1
Denis <TesslaDen@gmail.com>
2025-04-11 23:07:15 +0200

    generator and solver sudoku

---
+++ sudoku.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+const int SIZE = 9;     //board size
+
+bool isSafe(const vector<vector<int>>& board, int row, int col, int num) {
+    //checking the row
+    for (int x = 0; x < SIZE; ++x) {
+        if (board[row][x] == num) return false;
+    }
+    //checking the column
+    for (int x = 0; x < SIZE; ++x) {
+        if (board[x][col] == num) return false;
+    }
+
+    //checking the 3x3 block
+    int start_row = row - row % 3, start_col = col - col % 3;
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            if (board[i + start_row][j + start_col] == num) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+bool solve_sudoku(vector<vector<int>>& board) {
+    int row, col;
+    bool empty = false;
+
+    for (row = 0; row < SIZE; ++row) {
+        for (col = 0; col < SIZE; ++col) {
+            if (board[row][col] == 0) {
+                empty = true;
+                break;
+            }
+        }
+        if (empty) break;
+    }
+    if (!empty) return true;
+
+    for (int num = 1; num <= SIZE; ++num) {
+        if (isSafe(board, row, col, num)){
+            board[row][col] = num;
+            if (solve_sudoku(board)) {
+                return true;
+            }
+            board[row][col] = 0;
+        }
+    }
+    return false;
+}
+
+void generate_sudoku(vector<vector<int>>& board) {
+    solve_sudoku(board);
+
+    int emptyCells = 40;
+    while (emptyCells > 0) {
+        int row = rand() % SIZE;
+        int col = rand() % SIZE;
+
+        if (board[row][col] != 0) {
+            board[row][col] = 0;
+            --emptyCells;
+        }
+    }
+}
+
+//the function for the output of the board
+void print_board(const vector<vector<int>>& board) {
+    for (int row = 0; row < SIZE; ++row){
+        for (int col = 0; col < SIZE; ++col) {
+            cout << board[row][col] << " ";
+            //Dividers for 3x3 blocks
+            if ((col + 1) % 3 == 0 && col != SIZE - 1) cout << "| ";        
+        }
+        cout << endl;
+        //Dividers for 3x3 blocks
+        if ((row + 1) % 3 == 0 && row != SIZE - 1) cout << "---------------------" << endl;
+    }
+}
+
+int main()
+{
+    srand(time(0));
+    vector<vector<int>> board(SIZE, vector<int>(SIZE, 0));
+    generate_sudoku(board);
+    print_board(board);
+    return 0;
+}
+
+
