fe89bc1757dcd14dfc80291eef5af9357a12c214
Denis <TesslaDen@gmail.com>
2025-04-12 12:22:35 +0200

    added gameplay

--- sudoku.cpp
+++ sudoku.cpp
@@ -16,10 +16,12 @@ using namespace std;
 
 const int SIZE = 9;     //board size
 
+//difficulty levels
 enum class Difficulty {
     Easy, Medium, Hard
 };
 
+//checks whether it is possible to put num in the cell (row, col)
 bool isSafe(const vector<vector<int>>& board, int row, int col, int num) {
     //checking the row
     for (int x = 0; x < SIZE; ++x) {
@@ -32,7 +34,6 @@ bool isSafe(const vector<vector<int>>& board, int row, int col, int num) {
 
     //checking the 3x3 block
     int start_row = row - row % 3, start_col = col - col % 3;
-
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 3; ++j) {
             if (board[i + start_row][j + start_col] == num) {
@@ -43,11 +44,11 @@ bool isSafe(const vector<vector<int>>& board, int row, int col, int num) {
     return true;
 }
 
-//a function that solves Sudoku
+//a function that solves Sudoku with BackTracking
 bool solve_sudoku(vector<vector<int>>& board) {
     int row, col;
     bool empty = false;
-
+    //search for the first empty cell
     for (row = 0; row < SIZE; ++row) {
         for (col = 0; col < SIZE; ++col) {
             if (board[row][col] == 0) {
@@ -57,15 +58,17 @@ bool solve_sudoku(vector<vector<int>>& board) {
         }
         if (empty) break;
     }
+    //if there are no empty cells, Sudoku is solved.
     if (!empty) return true;
 
+    //try to put every number from 1 to 9
     for (int num = 1; num <= SIZE; ++num) {
         if (isSafe(board, row, col, num)){
             board[row][col] = num;
             if (solve_sudoku(board)) {
                 return true;
             }
-            board[row][col] = 0;
+            board[row][col] = 0;    //backtrack
         }
     }
     return false;
@@ -73,7 +76,7 @@ bool solve_sudoku(vector<vector<int>>& board) {
 
 //a function that generates Sudoku by removing some cells
 void generate_sudoku(vector<vector<int>>& board, Difficulty level) {
-    solve_sudoku(board);
+    solve_sudoku(board);    //creating a full board
 
     int emptyCells = 0;
     //select level
@@ -91,6 +94,8 @@ void generate_sudoku(vector<vector<int>>& board, Difficulty level) {
     default:
         break;
     }
+
+    //Deleting cells randomly
     while (emptyCells > 0) {
         int row = rand() % SIZE;
         int col = rand() % SIZE;
@@ -125,6 +130,48 @@ void print_board(const vector<vector<int>>& board) {
     }
 }
 
+// Check if the board is completely filled (No '0')
+bool is_board_full(const vector<vector<int>>& board) {
+    for (const auto& row : board)
+        for (int val : row)
+            if (val == 0) return false;
+    return true;
+}
+
+//game
+void play_game(vector<vector<int>>& board, const vector<vector<int>>& original) {
+    while (true) {
+        print_board(board);
+        int row, col, num;
+        cout << "Enter row (1-9), column (1-9), and number (1-9), or 0 0 0 to quit: ";
+        cin >> row >> col >> num;
+
+        if (row == 0 && col == 0 && num == 0) break;
+
+        --row; --col;
+        if (row < 0 || row >= SIZE || col < 0 || col >= SIZE || num < 1 || num > 9) {
+            cout << RED << "Invalid input." << RESET << endl;
+            continue;
+        }
+        if (original[row][col] != 0) {
+            cout << RED << "You can't change original cells!" << RESET << endl;
+            continue;
+        }
+        if (isSafe(board, row, col, num)) {
+            board[row][col] = num;
+        }
+        else {
+            cout << RED << "That number breaks Sudoku rules!" << RESET << endl;
+        }
+
+        if (is_board_full(board)) {
+            cout << GREEN << "Congratulations! You solved the puzzle!" << RESET << endl;
+            print_board(board);
+            break;
+        }
+    }
+}
+
 int main()
 {
     srand(time(0));
@@ -144,10 +191,12 @@ int main()
         cout << "Invalid choice. Defaulting to medium\n";
         break;
     }
-
+    solve_sudoku(board);
+    vector<vector<int>> solution = board;
     generate_sudoku(board, level);
+    vector<vector<int>> original = board;
     //solve_sudoku(board);
-    print_board(board);
+    play_game(board, original);
     return 0;
 }
 
